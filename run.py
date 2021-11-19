166f66f1b6e1bacb9497b7e248fca2d70c90aaa2
davewatters <89341297+davewatters@users.noreply.github.com>
2021-11-19 20:24:04 +0000

    add basic main screen

--- run.py
+++ run.py
@@ -1,3 +1,69 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import textwrap
+
+
+def cls():
+    '''
+    Clears the screen and places cursor at top left
+    Similar to *nix terminal clear or Win cls
+    '''
+    print("\033[H\033[J", end="")
+    return
+
+
+def disp_main_page():
+    '''
+    Displays the main greeting and menu options
+    '''
+    cls()
+    s = """
+        PwnyTrap - Catch bad pa$$words using the Have I Been Pwned API\n\n
+        Options:\n
+        1. Show Help screen\n
+        2. Check password\n
+        3. Check email address\n
+        4. Lookup breach info\n
+    """
+    print(textwrap.dedent(s))
+    return
+
+
+def help_screen():
+    '''
+    Displays the help information screen
+    '''
+    cls()
+    s = """
+        PwnyTrap Help\n
+
+        Lots of helpful info goes here.\n
+        TODO explain checking passowrd pwnage\n
+        TODO explain checking email address pwnage\n\n
+        TODO Explain the HIBP API\n
+        TODO Include a link to the HIBP site and also it's FAQ page\n
+
+        Finally, in case you were wondering...\n
+        PwnyTrap is pronounced 'Pony Trap'.  It comes from the leetspeak word 'pwn' meaning to be beaten or compromised in some way. 
+        For an explanation check this link: https://en.wikipedia.org/wiki/PWN\n
+    """
+    print(textwrap.dedent(s))
+    input("Any key to return to the main screen...")
+    return
+
+
+def main():
+    '''
+    Main program loop
+    '''
+    while True:
+        disp_main_page()
+        opt = input("Enter your choice [1-4, or 0 to quit]: ")
+        if opt == '0':
+            print('Goodbye.')
+            break
+        elif opt == '1':
+            help_screen()
+
+
+##
+main()
+##
